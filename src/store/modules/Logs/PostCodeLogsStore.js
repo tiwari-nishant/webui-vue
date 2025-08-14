df2f6c0a2fac4908fa5ef6d2e1f55f0aecee8872
Nikhil Ashoka <96164786+Nikhil-Ashoka@users.noreply.github.com>
2025-08-14 19:05:45 +0530

    Convert Hexa to ASCII in progress logs (#429)
    - Convert Hexa to ASCII in progress logs page.
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>

--- src/store/modules/Logs/PostCodeLogsStore.js
+++ src/store/modules/Logs/PostCodeLogsStore.js
@@ -20,11 +20,18 @@ const PostCodeLogsStore = {
           Members = Members.filter((log) => log.MessageArgs[3] !== '00000000');
           const postCodeLogs = Members.map((log) => {
             const { Created, MessageArgs, AdditionalDataURI } = log;
+            let asciiString = '';
+            let hexString = MessageArgs[2];
+            for (let i = 0; i < hexString.length; i += 2) {
+              const hexPair = hexString.substring(i, i + 2);
+              const decimalValue = parseInt(hexPair, 16);
+              asciiString += String.fromCharCode(decimalValue);
+            }
             return {
               date: new Date(Created),
               bootCount: MessageArgs[0],
               timeStampOffset: MessageArgs[1],
-              postCode: MessageArgs[3],
+              postCode: asciiString,
               uri: AdditionalDataURI,
             };
           });
