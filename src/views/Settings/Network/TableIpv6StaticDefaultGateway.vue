a07fe041085c583d48283e872d36baaa8cf14bc2
tiwari-nishant <152953092+tiwari-nishant@users.noreply.github.com>
2026-02-04 02:03:18 +0530

    Fixed table alignment for IPv6 default gateway address (#542)
    • Defect:https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=760210

--- src/views/Settings/Network/TableIpv6StaticDefaultGateway.vue
+++ src/views/Settings/Network/TableIpv6StaticDefaultGateway.vue
@@ -128,12 +128,6 @@ const ipv6DefaultGatewayTableFields = ref([
     tdAttr: { scope: null },
   },
   {
-    key: 'PrefixLength',
-    label: i18n.global.t('pageNetwork.table.prefixLength'),
-    thAttr: { scope: 'col' },
-    tdAttr: { scope: null },
-  },
-  {
     key: 'actions',
     label: '',
     tdClass: 'text-right',
@@ -265,5 +259,11 @@ const initIpv6DefaultGatewayModal = () => {
 <style lang="scss" scoped>
 .text-right {
   text-align: right;
+}
+
+:deep(.table) {
+  td {
+    white-space: nowrap;
+  }
 }
 </style>
