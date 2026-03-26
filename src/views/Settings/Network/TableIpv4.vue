bf47baae043e3a333c36af4348638f6c6c514d99
Nishant Tiwari <tiwari.nishant@ibm.com>
2026-03-26 15:54:39 +0530

    Fixed IPv4 DHCP toggle • Fixed element fetching via ref instead of id • Defect: https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=771912

--- src/views/Settings/Network/TableIpv4.vue
+++ src/views/Settings/Network/TableIpv4.vue
@@ -7,6 +7,7 @@
             <dt>{{ $t('pageNetwork.dhcp') }}</dt>
             <dd>
               <BFormCheckbox
+                ref="dhcpCheckboxRef"
                 :key="componentKey"
                 v-model="dhcpEnabledState"
                 data-test-id="networkSettings-switch-dhcpEnabled"
@@ -122,6 +123,7 @@ const props = defineProps({
 });
 
 const componentKey = ref(0);
+const dhcpCheckboxRef = ref(null);
 
 const openModal = ref(false);
 
@@ -364,9 +366,15 @@ const operationConfirm = () => {
 
 const operationCancel = () => {
   if (modalValue.value === 'ChangeDhcpEnabledState') {
-    let onDhcpCancel = document.getElementById('dhcpSwitch');
-    onDhcpCancel.checked = !modalPayload.value.state;
-    // Manually refresh the checkbox in DOM
+    if (dhcpCheckboxRef.value) {
+      const inputElement = dhcpCheckboxRef.value.$el?.querySelector(
+        'input[type="checkbox"]',
+      );
+      if (inputElement) {
+        inputElement.checked = !modalPayload.value.state;
+      }
+    }
+    // Force component re-render to sync with the reverted state
     componentKey.value += 1;
   }
 };
