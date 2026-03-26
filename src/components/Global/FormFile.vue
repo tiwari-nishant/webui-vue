b0a0ad10354335936819331177c146f25f7962da
vedangimittal <69679625+vedangimittal@users.noreply.github.com>
2026-03-26 21:44:40 +0530

    Fixed error message placement (#592)
    - Fixed error message placement on Certificates page
    - Defect: https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=772278
    
    Signed-off-by: Vedangi Mittal <vedangimittal3004@gmail.com>

--- src/components/Global/FormFile.vue
+++ src/components/Global/FormFile.vue
@@ -22,8 +22,10 @@
       >
         {{ $t('global.fileUpload.browseText') }}
       </span>
-      <slot name="invalid"></slot>
     </label>
+    <div class="error-message-wrapper">
+      <slot name="invalid"></slot>
+    </div>
     <div v-if="file" class="clear-selected-file px-3 py-2 mt-2">
       {{ file ? file.name : '' }}
       <BButton
@@ -123,7 +125,19 @@ eventBus.on('clear-file', () => {
   height: 56px;
   width: 90px;
 }
+.error-message-wrapper {
+  display: block;
+  width: 100%;
+  margin-top: 0.25rem;
+  clear: both;
+}
 .custom-form-file-container {
   word-break: break-all;
+}
+// Only show invalid feedback when form control is actually invalid
+.custom-form-file-container:has(.form-control-file.is-invalid)
+  .error-message-wrapper
+  :deep(.invalid-feedback) {
+  display: block;
 }
 </style>
