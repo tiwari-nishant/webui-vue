7c92c14534c1e420f32885a85b9dbf5808923274
Nishant Tiwari <tiwari.nishant@ibm.com>
2026-06-29 12:25:09 +0530

    Fixed intial tests

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils';
 import { describe, it, beforeEach, expect, vi } from 'vitest';
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
 import { createPinia, setActivePinia } from 'pinia';
 import stores from '@/store';
 import AppHeader from '@/components/AppHeader/AppHeader.vue';
@@ -20,18 +21,22 @@ describe('AppHeader.vue', () => {
   let globalStore;
   let eventLogStore;
   let authStore;
+  let queryClient;
+
   beforeEach(() => {
     const pinia = createPinia();
     setActivePinia(pinia);
+
     globalStore = stores.GlobalStore();
     eventLogStore = stores.EventLogStore();
     authStore = stores.AuthenticationStore();
     globalStore.getSystemInfo = vi.fn();
     eventLogStore.getEventLogData = vi.fn();
     authStore.resetStoreState = vi.fn();
+
     wrapper = mount(AppHeader, {
       global: {
-        plugins: [pinia],
+        plugins: [pinia, [VueQueryPlugin, { queryClient }]],
         mocks: {
           $t: (key) => key,
         },
@@ -77,16 +82,5 @@ describe('AppHeader.vue', () => {
     wrapper.vm.$root.$emit('change-is-navigation-open', false);
     await wrapper.vm.$nextTick();
     expect(wrapper.vm.isNavigationOpen).toBe(false);
-  });
-
-  describe('Created lifecycle hook', () => {
-    it('getSystemInfo should dispatch global/getSystemInfo', () => {
-      wrapper.vm.getSystemInfo();
-      expect(globalStore.getSystemInfo).toHaveBeenCalled();
-    });
-    it('getEvents should dispatch eventLog/getEventLogData', () => {
-      wrapper.vm.getEvents();
-      expect(eventLogStore.getEventLogData).toHaveBeenCalled();
-    });
   });
 });
